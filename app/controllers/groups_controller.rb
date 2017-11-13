bcd3a6baaae43471d50d9408c3ad10f4d921af03
duartisan <duanzhengyang@duanzhengyangdeMacBook-Pro.local>
2017-11-13 18:32:55 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path, notice: "更新成功"
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "讨论组删除成功"
+    redirect_to groups_path
+  end
+
   private
 
   def group_params
